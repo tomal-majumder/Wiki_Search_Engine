a09e76d0d029bca28cda6bbf9fa84a356c569ac1
tomal-majumder <tomalmajumder094@gmail.com>
2025-07-30 19:01:00 -0700

    Update CI/CD scripts

---
+++ react-app/build/precache-manifest.f5cb50ea45e24c06754cac03ad638413.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c375a9ac69d35bff16365549d723ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc2611de5ddab3a94811",
+    "url": "/static/css/2.5417e51f.chunk.css"
+  },
+  {
+    "revision": "132dbbfe88af4651d758",
+    "url": "/static/css/main.c3746e04.chunk.css"
+  },
+  {
+    "revision": "bc2611de5ddab3a94811",
+    "url": "/static/js/2.18232865.chunk.js"
+  },
+  {
+    "revision": "87dcf563f624c5022b95",
+    "url": "/static/js/3.32c3e341.chunk.js"
+  },
+  {
+    "revision": "132dbbfe88af4651d758",
+    "url": "/static/js/main.42913c1d.chunk.js"
+  },
+  {
+    "revision": "c9f744a2f833ebcedb15",
+    "url": "/static/js/runtime~main.e52c2781.js"
+  }
+]);
